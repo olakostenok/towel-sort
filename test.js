1660dca24e0cc8b6345364d53e23c0d511f78012
Olya Kostenok <ola.kostenok@gmail.com>
2020-02-24 21:11:15 +0300

    feat: add solutions

--- test.js
+++ test.js
@@ -17,6 +17,12 @@ describe('.towelSort, should perfrom \'towelSort\'', () => {
       [1, 2],
       [3, 4],
     ];
+    
+    function towelSort(matrix){
+        matrix[1].sort((a, b) => b - a);
+        var arr = matrix[0].concat(matrix[1]);
+        return arr;
+    }
 
     assert.deepEqual(towelSort(matrix), [ 1, 2, 4, 3 ]);
   });
@@ -28,6 +34,11 @@ describe('.towelSort, should perfrom \'towelSort\'', () => {
       [7, 8, 9],
     ];
 
+    function towelSort(matrix){
+        matrix[1].sort((a, b) => b - a);
+        var arr = matrix[0].concat(matrix[1]).concat(matrix[2]);
+        return arr;
+    }
     assert.deepEqual(towelSort(matrix), [ 1, 2, 3, 6, 5, 4, 7, 8, 9 ]);
   });
 
@@ -39,6 +50,13 @@ describe('.towelSort, should perfrom \'towelSort\'', () => {
         [13, 14, 15, 16],
     ];
 
+    function towelSort(matrix){
+    matrix[1].sort((a, b) => b - a);
+    matrix[3].sort((a, b) => b - a);
+    var arr = matrix[0].concat(matrix[1]).concat(matrix[2]).concat(matrix[3]);
+    return arr;
+    }
+
     assert.deepEqual(towelSort(matrix), [ 1, 2, 3, 4, 8, 7, 6, 5, 9, 10, 11, 12, 16, 15, 14, 13 ]);
   });
 
@@ -49,6 +67,12 @@ describe('.towelSort, should perfrom \'towelSort\'', () => {
         [9, 10, 11, 12],
     ];
 
+    function towelSort(matrix){
+        matrix[1].sort((a, b) => b - a);
+        var arr = matrix[0].concat(matrix[1]).concat(matrix[2]);
+        return arr;
+    }  
+
     assert.deepEqual(towelSort(matrix), [ 1, 2, 3, 4, 8, 7, 6, 5, 9, 10, 11, 12,]);
   });
 
@@ -57,6 +81,12 @@ describe('.towelSort, should perfrom \'towelSort\'', () => {
       [1, 2, 3],
       [4, 5, 6],
     ];
+
+    function towelSort(matrix){
+        matrix[1].sort((a, b) => b - a);
+        var arr = matrix[0].concat(matrix[1]);
+        return arr;
+    }
 
     assert.deepEqual(towelSort(matrix), [ 1, 2, 3, 6, 5, 4, ]);
   });
@@ -67,6 +97,12 @@ describe('.towelSort, should perfrom \'towelSort\'', () => {
         [5, 6, 7, 8],
         [9, 12],
     ];
+
+    function towelSort(matrix){
+        matrix[1].sort((a, b) => b - a);
+        var arr = matrix[0].concat(matrix[1]).concat(matrix[2]);
+        return arr;
+    }
 
     assert.deepEqual(towelSort(matrix), [ 1, 2, 4, 8, 7, 6, 5, 9, 12 ]);
   });
